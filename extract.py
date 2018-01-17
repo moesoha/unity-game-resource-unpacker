21fdd09980f17f2cfc650f63acf0831984faa90d
Soha Jin <soha@lohu.info>
2018-01-17 13:51:16 +0800

    optimize

--- extract.py
+++ extract.py
@@ -1,10 +1,7 @@
-apkExtractedPath="C:\\Users\\soha\\com.papegames.evol_unpkg\\"
-assetsExtractTo="./assets"
-
 import unitypack
-import fsb5,PIL
+import fsb5
 from PIL import ImageOps,Image
-import os,pickle,io,sys
+import os,pickle,io,sys,locale
 from unitypack.engine.texture import TextureFormat
 from argparse import ArgumentParser
 import subprocess
@@ -22,17 +19,16 @@ ETC_SERIES=(
 	TextureFormat.EAC_RG_SIGNED,
 )
 
-def parseCmdArgs(args):
-	parser=ArgumentParser()
-	parser.add_argument("-o", "--outdir",nargs="?",default="",help="Directory where extracted files will be put",required=True)
-	parser.add_argument("-i", "--indir",nargs="?",default="",help="Directory to parse",required=True)
-	args=parser.parse_args(args)
-	if(args.indir=='' or args.outdir==''):
-		parser.print_help()
-		exit()
-	else:
-		apkExtractedPath=args.indir
-		assetsExtractTo=args.outdir
+def putTextFile(filename,directory,content,mode="wb",encoding="utf-8"):
+	if(not(os.path.exists(directory))):
+		os.makedirs(directory)
+
+	path=os.path.join(directory,filename)
+
+	with open(path,mode=mode,encoding=encoding) as file:
+		print("Writing contents to "+path)
+		file.write(content)
+	return path
 
 def putFile(filename,directory,content,mode="wb"):
 	if(not(os.path.exists(directory))):
@@ -41,6 +37,7 @@ def putFile(filename,directory,content,mode="wb"):
 	path=os.path.join(directory,filename)
 
 	with open(path,mode=mode) as file:
+		print("Writing contents to "+path)
 		file.write(content)
 	return path
 
@@ -81,89 +78,114 @@ def getPKMHeader(width,height,tformat):
 
 	return(header+version+formatB+widthB+heightB+widthB+heightB)
 
-def handleFile(filepath):
-	subdirname=filepath.replace(apkExtractedPath,"")
-	savepath=os.path.join(assetsExtractTo,subdirname)
-	os.makedirs(savepath,exist_ok=True)
-	# savepath=os.path.dirname(filepath);
-	with open(filepath,'rb') as file:
-		bundle=unitypack.load(file)
-		for asset in bundle.assets:
-			for id,obj in asset.objects.items():
-				try:
-					data=obj.read()
+class UnityGameResUnpack:
 
-					if obj.type=="AudioClip":
-						print(obj.type,":",data.name)
-						# extract samples
-						bindata=data.data
-						index=0
-						while bindata:
-							fsb=fsb5.load(bindata)
-							ext=fsb.get_sample_extension()
-							bindata=bindata[fsb.raw_size:]
-							for sampleName,sampleData in readSamplesFromFSB5(fsb):
-								filenameWrite=data.name+"--"+sampleName+'.'+ext
-								putFile(filenameWrite,savepath,sampleData)
-							index+=1
-					
-					elif obj.type=="Texture2D":
-						print(obj.type+"["+str(data.format)+"]:",data.name)
-						filename=data.name+".png"
-						if data.format in ETC_SERIES:
-							bindata=getPKMHeader(data.width,data.height,data.format)+data.image_data
-							putFile('temp.pkm',currentWorkDir,bindata)
-							subprocess.call(["./etcpack","./temp.pkm",currentWorkDir])
-							image=Image.open('./temp.ppm')
-						else:
-							image=data.image
-							if image is None:
-								print("WARNING: %s is an empty image"%(filename))
-								continue
-						img=ImageOps.flip(image)
-						output=io.BytesIO()
-						img.save(output,format="png")
-						putFile(filename,savepath,output.getvalue())
+	apkExtractedPath="./orz/"
+	assetsExtractTo="./assets/"
 
-					elif obj.type=="MovieTexture":
-						print(obj.type,":",data.name)
-						filename=data.name+".ogv"
-						putFile(filename,savepath,data.movie_data)
+	def __init__(self,args):
+		self.parseCmdArgs(args)
 
-					# elif obj.type=="Shader":
-					# 	print(obj.type,":",data.name)
-					# 	filename=data.name+".cg"
-					# 	putFile(filename,savepath,data.script)
-					
-					elif obj.type=="Mesh":
-						print(obj.type,":",data.name)
-						try:
-							meshdata=unitypack.export.OBJMesh(d).export()
-							filename=data.name+".obj"
-							putFile(filename,savepath,meshdata)
-						except NotImplementedError as e:
-							print("WARNING: Could not extract %r (%s)"%(d,e))
-							meshdata=pickle.dumps(data._obj)
-							filename=data.name+".Mesh.pickle"
-							putFile(filename,savepath,meshdata)
-					
-					elif obj.type=="Font":
-						print(obj.type,":",data.name)
-						filename=data.name+".ttf"
-						putFile(filename,savepath,data.data)
+	def parseCmdArgs(self,args):
+		parser=ArgumentParser()
+		parser.add_argument("-o", "--outdir",nargs="?",default="",help="Directory where extracted files will be put",required=True)
+		parser.add_argument("-i", "--indir",nargs="?",default="",help="Directory to parse",required=True)
+		args=parser.parse_args(args)
+		if(args.indir=='' or args.outdir==''):
+			parser.print_help()
+			exit()
+		else:
+			self.apkExtractedPath=args.indir
+			self.assetsExtractTo=args.outdir
+			print(args.indir,args.outdir)
 
-					elif obj.type=="TextAsset":
-						print(obj.type,":",data.name)
-						if isinstance(data.script,bytes):
-							filename,mode=data.name+".bin","wb"
-						else:
-							filename,mode=data.name+".txt","w"
-						putFile(filename,savepath,data.script,mode=mode)
-				except Exception as e:
-					print("WARNING: Error while processing %r (%s)"%(filepath,e))
+
+	def handleFile(self,filepath):
+		subdirname=filepath.replace(self.apkExtractedPath,"")
+		while(subdirname[0]=="/" or subdirname[0]=="\\"):
+			subdirname=subdirname[1:]
+		savepath=os.path.join(self.assetsExtractTo,subdirname)
+		os.makedirs(savepath,exist_ok=True)
+		# savepath=os.path.dirname(filepath);
+		with open(filepath,'rb') as file:
+			bundle=unitypack.load(file)
+			for asset in bundle.assets:
+				for id,obj in asset.objects.items():
+					try:
+						data=obj.read()
+
+						if obj.type=="AudioClip":
+							print(obj.type,":",data.name)
+							# extract samples
+							bindata=data.data
+							index=0
+							while bindata:
+								fsb=fsb5.load(bindata)
+								ext=fsb.get_sample_extension()
+								bindata=bindata[fsb.raw_size:]
+								for sampleName,sampleData in readSamplesFromFSB5(fsb):
+									filenameWrite=data.name+"--"+sampleName+'.'+ext
+									putFile(filenameWrite,savepath,sampleData)
+								index+=1
+						
+						elif obj.type=="Texture2D":
+							print(obj.type+"["+str(data.format)+"]:",data.name)
+							filename=data.name+".png"
+							if data.format in ETC_SERIES:
+								bindata=getPKMHeader(data.width,data.height,data.format)+data.image_data
+								putFile('temp.pkm',currentWorkDir,bindata)
+								subprocess.call(["./etcpack","./temp.pkm",currentWorkDir])
+								image=Image.open('./temp.ppm')
+							else:
+								image=data.image
+								if image is None:
+									print("WARNING: %s is an empty image"%(filename))
+									continue
+							img=ImageOps.flip(image)
+							output=io.BytesIO()
+							img.save(output,format="png")
+							putFile(filename,savepath,output.getvalue())
+
+						elif obj.type=="MovieTexture":
+							print(obj.type,":",data.name)
+							filename=data.name+".ogv"
+							putFile(filename,savepath,data.movie_data)
+
+						# elif obj.type=="Shader":
+						# 	print(obj.type,":",data.name)
+						# 	filename=data.name+".cg"
+						# 	putFile(filename,savepath,data.script)
+						
+						elif obj.type=="Mesh":
+							print(obj.type,":",data.name)
+							try:
+								meshdata=unitypack.export.OBJMesh(d).export()
+								filename=data.name+".obj"
+								putFile(filename,savepath,meshdata)
+							except NotImplementedError as e:
+								print("WARNING: Could not extract %r (%s)"%(d,e))
+								meshdata=pickle.dumps(data._obj)
+								filename=data.name+".Mesh.pickle"
+								putFile(filename,savepath,meshdata)
+						
+						elif obj.type=="Font":
+							print(obj.type,":",data.name)
+							filename=data.name+".ttf"
+							putFile(filename,savepath,data.data)
+
+						elif obj.type=="TextAsset":
+							print(obj.type,":",data.name)
+							if isinstance(data.script,bytes):
+								filename,mode=data.name,"wb"
+							else:
+								filename,mode=data.name,"w"
+							putTextFile(filename,savepath,data.script,mode=mode,encoding="utf-8")
+					except Exception as e:
+						print("WARNING: Error while processing %r (%s)"%(filepath,e))
 
 def main():
-	for currentPath,dirs,files in os.walk(apkExtractedPath):
+	ugru=UnityGameResUnpack(sys.argv[1:])
+	for currentPath,dirs,files in os.walk(ugru.apkExtractedPath):
 		for nowFile in files:
 			try:
 				nowFilePath=os.path.join(currentPath,nowFile)
@@ -172,9 +194,8 @@ def main():
 			except NotImplementedError as e:
 				pass
 			else:
-				handleFile(nowFilePath)
+				ugru.handleFile(nowFilePath)
 
 
 if __name__=="__main__":
-	parseCmdArgs(sys.argv[1:])
 	main()
